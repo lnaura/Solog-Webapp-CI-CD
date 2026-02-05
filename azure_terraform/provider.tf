bb906343c6f11397e4ea3dfa8c804ddf598c30a4
jiwon <lnaura@naver.com>
2026-02-05 20:38:14 +0900

    fix

--- azure_terraform/provider.tf
+++ azure_terraform/provider.tf
@@ -20,7 +20,11 @@ terraform {
 provider "azurerm" {
   resource_provider_registrations = "none"
   subscription_id = var.subscription_id
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 provider "kubernetes" {
